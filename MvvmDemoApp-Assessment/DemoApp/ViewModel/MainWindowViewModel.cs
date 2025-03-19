97bafedb99d0453f09646e86f41c235b1aa4e698
Sandeep Shenoy <s.sandeep@noxum.com>
2025-03-19 10:29:05 +0530

    Initial Commit

---
+++ MvvmDemoApp-Assessment/DemoApp/ViewModel/MainWindowViewModel.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+using DemoApp.DataAccess;
+using DemoApp.Model;
+using DemoApp.Properties;
+
+namespace DemoApp.ViewModel
+{
+	/// <summary>
+	/// The ViewModel for the application's main window.
+	/// </summary>
+	public class MainWindowViewModel : WorkspaceViewModel
+	{
+		#region Fields
+
+		ReadOnlyCollection<CommandViewModel> _commands;
+		readonly CustomerRepository _customerRepository;
+		ObservableCollection<WorkspaceViewModel> _workspaces;
+
+		#endregion // Fields
+
+		#region Constructor
+
+		public MainWindowViewModel(string customerDataFile)
+		{
+			base.DisplayName = Strings.MainWindowViewModel_DisplayName;
+
+			_customerRepository = new CustomerRepository(customerDataFile);
+		}
+
+		#endregion // Constructor
+
+		#region Commands
+
+		/// <summary>
+		/// Returns a read-only list of commands 
+		/// that the UI can display and execute.
+		/// </summary>
+		public ReadOnlyCollection<CommandViewModel> Commands
+		{
+			get
+			{
+				if (_commands == null)
+				{
+					List<CommandViewModel> cmds = this.CreateCommands();
+					_commands = new ReadOnlyCollection<CommandViewModel>(cmds);
+				}
+				return _commands;
+			}
+		}
+
+		List<CommandViewModel> CreateCommands()
+		{
+			return new List<CommandViewModel>
+						{
+								new CommandViewModel(
+										Strings.MainWindowViewModel_Command_ViewAllCustomers,
+										new RelayCommand(param => this.ShowAllCustomers())),
+
+								new CommandViewModel(
+										Strings.MainWindowViewModel_Command_Sales,
+										new RelayCommand(param => this.CreateSales())),
+
+									new CommandViewModel(
+										Strings.MainWindowViewModel_Command_CreateNewCustomer,
+										new RelayCommand(param => this.CreateNewCustomer()))
+						};
+		}
+
+		private void CreateSales()
+		{
+			throw new NotImplementedException();
+		}
+
+		#endregion // Commands
+
+		#region Workspaces
+
+		/// <summary>
+		/// Returns the collection of available workspaces to display.
+		/// A 'workspace' is a ViewModel that can request to be closed.
+		/// </summary>
+		public ObservableCollection<WorkspaceViewModel> Workspaces
+		{
+			get
+			{
+				if (_workspaces == null)
+				{
+					_workspaces = new ObservableCollection<WorkspaceViewModel>();
+					_workspaces.CollectionChanged += this.OnWorkspacesChanged;
+				}
+				return _workspaces;
+			}
+		}
+
+		void OnWorkspacesChanged(object sender, NotifyCollectionChangedEventArgs e)
+		{
+			if (e.NewItems != null && e.NewItems.Count != 0)
+				foreach (WorkspaceViewModel workspace in e.NewItems)
+					workspace.RequestClose += this.OnWorkspaceRequestClose;
+
+			if (e.OldItems != null && e.OldItems.Count != 0)
+				foreach (WorkspaceViewModel workspace in e.OldItems)
+					workspace.RequestClose -= this.OnWorkspaceRequestClose;
+		}
+
+		void OnWorkspaceRequestClose(object sender, EventArgs e)
+		{
+			WorkspaceViewModel workspace = sender as WorkspaceViewModel;
+			workspace.Dispose();
+			this.Workspaces.Remove(workspace);
+		}
+
+		#endregion // Workspaces
+
+		#region Private Helpers
+
+		void CreateNewCustomer()
+		{
+			try
+			{
+				Customer newCustomer = Customer.CreateNewCustomer();
+				CustomerViewModel workspace = new CustomerViewModel(null, _customerRepository);
+				this.Workspaces.Add(workspace);
+				this.SetActiveWorkspace(workspace);
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.ToString());
+			}
+		}
+
+		void ShowAllCustomers()
+		{
+			AllCustomersViewModel workspace =
+					this.Workspaces.FirstOrDefault(vm => vm is AllCustomersViewModel)
+					as AllCustomersViewModel;
+
+			if (workspace == null)
+			{
+				workspace = new AllCustomersViewModel(_customerRepository);
+				this.Workspaces.Add(workspace);
+			}
+
+			this.SetActiveWorkspace(workspace);
+		}
+
+		void SetActiveWorkspace(WorkspaceViewModel workspace)
+		{
+			Debug.Assert(this.Workspaces.Contains(workspace));
+
+			ICollectionView collectionView = CollectionViewSource.GetDefaultView(this.Workspaces);
+			if (collectionView != null)
+				collectionView.MoveCurrentTo(workspace);
+		}
+
+		#endregion // Private Helpers
+	}
+}
